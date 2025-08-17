7d502865ad0ed850efd1afb175fb3e1c24209186
saurxbh <36542272+saurxbh@users.noreply.github.com>
2025-08-16 20:13:33 -0400

    Get scraper service working

--- src/main/java/com/jobscraper/job_scraper/controller/ScraperController.java
+++ src/main/java/com/jobscraper/job_scraper/controller/ScraperController.java
@@ -1,4 +1,21 @@
 package com.jobscraper.job_scraper.controller;
 
+import com.jobscraper.job_scraper.service.JobScraperService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+
+@RestController
+@RequestMapping("/api/scraper")
 public class ScraperController {
+
+    @Autowired
+    private JobScraperService scraperService;
+
+    @GetMapping("/run")
+    public String runScraper() {
+        scraperService.scrapeAllCompanies();
+        return "Scraper executed!";
+    }
 }
