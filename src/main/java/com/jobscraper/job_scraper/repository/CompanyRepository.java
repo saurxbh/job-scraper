727819ee62c0ba807d902f57c887370571c74c09
saurxbh <36542272+saurxbh@users.noreply.github.com>
2025-08-17 00:30:42 -0400

    Switch H2 out to Postgres

--- src/main/java/com/jobscraper/job_scraper/repository/CompanyRepository.java
+++ src/main/java/com/jobscraper/job_scraper/repository/CompanyRepository.java
@@ -1,4 +1,7 @@
 package com.jobscraper.job_scraper.repository;
 
-public class CompanyRepository {
+import com.jobscraper.job_scraper.entity.Company;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CompanyRepository extends JpaRepository<Company, Long> {
 }
--- src/main/java/com/jobscraper/job_scraper/repository/JobRepository.java
+++ src/main/java/com/jobscraper/job_scraper/repository/JobRepository.java
@@ -1,4 +1,10 @@
 package com.jobscraper.job_scraper.repository;
 
-public class JobRepository {
+import com.jobscraper.job_scraper.entity.Job;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface JobRepository extends JpaRepository<Job, Long> {
+
 }
