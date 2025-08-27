27d78f990a26537cd61867ed750442aa85f48c1b
saurxbh <36542272+saurxbh@users.noreply.github.com>
2025-08-26 22:45:02 -0400

    Add config for Kafka

--- src/main/java/com/jobscraper/job_scraper/config/KafkaConfig.java
+++ src/main/java/com/jobscraper/job_scraper/config/KafkaConfig.java
@@ -1,4 +1,16 @@
 package com.jobscraper.job_scraper.config;
 
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.KafkaTemplate;
+
+@Configuration
 public class KafkaConfig {
+    public static final String TOPIC = "jobs.scrape.completed";
+
+    @Bean
+    public NewTopic scrapeCompletedTopic() {
+        return new NewTopic(TOPIC, 1, (short)1);
+    }
 }
